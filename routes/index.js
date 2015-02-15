810c431a40cb9bde9348e34e304c1c992e081400
kkkon <kiyofumi.kondoh+github@gmail.com>
2015-02-15 11:48:43 +0900

    implement check cookie

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.cookie('checkCookie', 1 );
+
+  res.redirect('checkCookie');
+
+});
+
+/* check cookie */
+router.get('/checkCookie', function(req, res, next) {
+  if (req.cookies.checkCookie)
+  {
+    res.redirect('index');
+  }
+  else
+  {
+    res.redirect('/error?result=cookie');
+  }
+});
+
+/* GET home page. */
+router.get('/index', function(req, res, next) {
+  if (req.cookies.checkCookie)
+  {
+    res.render('index', { title: 'Express' });
+  }
+  else
+  {
+    res.redirect('/error?result=cookie');
+  }
 });
 
 module.exports = router;
