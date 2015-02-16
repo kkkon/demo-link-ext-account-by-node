18ab7466f289b8c53c306ef73debe67fb86ded4b
kkkon <kiyofumi.kondoh+github@gmail.com>
2015-02-16 22:16:28 +0900

    add parameter check at entry, come from application

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
+var checkEntryParam = function(req, res, next) {
+  var mode;
+
+  if (req.query.mode)
+  {
+    mode = String(req.query.mode);
+    if (
+      'link' === mode
+      || 'recovery' === mode
+    )
+    {
+      req.session.ext_account_mode = mode;
+    }
+    else
+    {
+      mode = null;
+    }
+  }
+  if (!mode)
+  {
+    return res.redirect('error?result=mode');
+  }
+
+  if ( 'link' === mode )
+  {
+    var appuid;
+
+    if (req.query.appuid)
+    {
+      appuid = String(req.query.appuid);
+      req.session.appuid = appuid;
+    }
+
+    if (!appuid)
+    {
+      return res.redirect('error?result=appuid');
+    }
+  }
+
+  next();
+};
+
 var checkCookie = function(req, res, next) {
   if (req.cookies.checkCookie)
   {
@@ -12,7 +54,7 @@ var checkCookie = function(req, res, next) {
   }
 };
 
-router.get('/', function(req, res, next) {
+router.get('/', checkEntryParam, function(req, res, next) {
   res.cookie('checkCookie', 1 );
 
   res.redirect('checkCookie');
