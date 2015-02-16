27729963ca27461b252890189de85f9d122d25ec
kkkon <kiyofumi.kondoh+github@gmail.com>
2015-02-16 18:55:57 +0900

    add test models/userextaccounts

---
+++ test/models/userextaccounts.test.js
@@ -0,0 +1,49 @@
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+var config = require('config');
+
+require('./../../models/userextaccount');
+
+var db = mongoose.connection;
+db.on('error', function(err) {
+  console.log('Mongoose connection error: ' + err);
+  mongoose.disconnect();
+});
+db.once('open', function() {
+  setTimeout( function() {
+    mongoose.disconnect();
+  }, 3000 );
+});
+mongoose.disconnect();
+mongoose.connect(config.db.url);
+
+var UserExtAccount = mongoose.model('UserExtAccount');
+
+function randomValueHex(len) {
+  return crypto.randomBytes(Math.ceil(len/2))
+    .toString('hex')
+    .slice(0,len);
+};
+
+function randomValueDec(len) {
+  return crypto.randomBytes(Math.ceil(len/2))
+    .toString('dec');
+};
+
+describe('UserExtAccounts', function() {
+
+  it('regist a new user', function(done) {
+    var appuid = randomValueHex(8);
+    var aid = randomValueHex(10);
+
+    var user = new UserExtAccount({
+      uid: appuid
+      , google: { id: aid }
+    });
+    user.save( function(err) {
+      if (err) { console.log(err); }
+      done();
+    });
+  });
+
+});
